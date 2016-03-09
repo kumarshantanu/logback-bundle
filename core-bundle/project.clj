da8cb3a781a6ae4b8d7e6d7decc295bcddad7985
Shantanu Kumar <kumar.shantanu@gmail.com>
2016-03-09 16:45:18 +0530

    begin 0.2.1 development

--- core-bundle/project.clj
+++ core-bundle/project.clj
@@ -1,18 +1,17 @@
-(defproject logback-bundle/core-bundle "0.2.0"
+(defproject logback-bundle/core-bundle "0.2.1-SNAPSHOT"
   :description "Core Logback dependencies bundle"
   :url "https://github.com/kumarshantanu/logback-bundle"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.codehaus.janino/janino     "2.7.8"]  ; for conditional config processing
-                 [ch.qos.logback/logback-classic "1.1.3"]
-                 [ch.qos.logback/logback-core    "1.1.3"]]
+                 [ch.qos.logback/logback-classic "1.1.6"]
+                 [ch.qos.logback/logback-core    "1.1.6"]]
   :java-source-paths ["java-src"]
   :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
   :global-vars {*warn-on-reflection* true
                 *unchecked-math* :warn-on-boxed}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
-                                  [cambium             "0.2.0"]
+                                  [cambium             "0.3.0"]
                                   ;;[org.slf4j/slf4j-api "1.7.12"]
                                   ]
                    :jvm-opts ["-Denable.dummy=true"]}})
-
--- json-bundle/project.clj
+++ json-bundle/project.clj
@@ -1,9 +1,9 @@
-(defproject logback-bundle/json-bundle "0.2.0"
+(defproject logback-bundle/json-bundle "0.2.1-SNAPSHOT"
   :description "Core and JSON Logback dependencies bundle"
   :url "https://github.com/kumarshantanu/logback-bundle"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[logback-bundle/core-bundle "0.2.0"]
+  :dependencies [[logback-bundle/core-bundle "0.2.1-SNAPSHOT"]
                  [com.fasterxml.jackson.core/jackson-core     "2.5.3"]  ; in use by cheshire 5.5.0
                  [com.fasterxml.jackson.core/jackson-databind "2.5.3"]  ; in use by cheshire 5.5.0
                  [ch.qos.logback.contrib/logback-json-classic "0.1.2"]
@@ -13,6 +13,6 @@
   :global-vars {*warn-on-reflection* true
                 *unchecked-math* :warn-on-boxed}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
-                                  [cambium             "0.2.0"]
+                                  [cambium             "0.3.0"]
                                   [org.slf4j/slf4j-api "1.7.12"]]}})
 
