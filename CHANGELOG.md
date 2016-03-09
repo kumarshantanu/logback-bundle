cd9b89aaef942e92617deaf6d9753eb43aea6674
Shantanu Kumar <kumar.shantanu@gmail.com>
2016-03-09 16:49:44 +0530

    bump version to 0.2.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 All notable changes to this project will be documented in this file.
 
 
+## [0.2.1] - 2016-March-09
+
+- Upgrade logback classic/core dependencies to version `1.1.6`
+- Allow `logback_bundle.json.FlatJsonlayout` to set a global MDC value decoder
+
+
 ## [0.2.0] - 2016-February-12
 - Strategy based TurboFilter extension mechanism
   - Override log level at runtime via MDC key/value
--- json-bundle/project.clj
+++ json-bundle/project.clj
@@ -1,9 +1,9 @@
-(defproject logback-bundle/json-bundle "0.2.1-SNAPSHOT"
+(defproject logback-bundle/json-bundle "0.2.1"
   :description "Core and JSON Logback dependencies bundle"
   :url "https://github.com/kumarshantanu/logback-bundle"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[logback-bundle/core-bundle "0.2.1-SNAPSHOT"]
+  :dependencies [[logback-bundle/core-bundle "0.2.1"]
                  [com.fasterxml.jackson.core/jackson-core     "2.5.3"]  ; in use by cheshire 5.5.0
                  [com.fasterxml.jackson.core/jackson-databind "2.5.3"]  ; in use by cheshire 5.5.0
                  [ch.qos.logback.contrib/logback-json-classic "0.1.2"]
@@ -15,4 +15,3 @@
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
                                   [cambium             "0.3.0"]
                                   [org.slf4j/slf4j-api "1.7.12"]]}})
-
