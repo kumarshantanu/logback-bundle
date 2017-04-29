cd2018152a62d109dee27c3479f7de3ed9999241
Shantanu Kumar <kumar.shantanu@gmail.com>
2017-04-29 19:27:40 +0530

    update core-bundle test dependency from cambium 0.8.1 to 0.9.0

--- core-bundle/project.clj
+++ core-bundle/project.clj
@@ -10,8 +10,7 @@
   :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
   :global-vars {*warn-on-reflection* true
                 *unchecked-math* :warn-on-boxed}
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
-                                  [cambium             "0.8.1"]
-                                  ;;[org.slf4j/slf4j-api "1.7.22"]
-                                  ]
+  :profiles {:dev {:dependencies [[org.clojure/clojure  "1.8.0"]
+                                  [cambium/cambium.core "0.9.0"]  ; pulls in [org.slf4j/slf4j-api "1.7.25"]
+                                  [cambium/cambium.codec-simple "0.9.0"]]
                    :jvm-opts ["-Denable.dummy=true"]}})
